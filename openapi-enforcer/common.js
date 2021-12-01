9727bfd5944c8c9bd950ccb48d02752943e22f60
James Speirs <james.speirs@gmail.com>
2021-11-30 17:47:53 -0700

    add reusable code

---
+++ openapi-enforcer/common.js
@@ -0,0 +1,13 @@
+module.exports = {
+  ecosystem: {
+    text: 'Ecosystem',
+    items: [
+      { text: 'AWS Lambda', link: 'https://aws-lambda.openapi-enforcer.com' },
+      { text: 'Express Middleware', link: 'https://middleware.openapi-enforcer.com' },
+    ]
+  },
+  versions: [
+    { text: 'v1.x', link: 'https://openapi-enforcer.com' },
+    { text: 'v2.x', link: 'https://v2.openapi-enforcer.com' }
+  ]
+}
